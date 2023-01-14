e49ffbf6aa3692ac17eb5e1673a85d72ee2626f6
Vitor Rafael <rqfvitor@gmail.com>
2023-01-14 20:05:13 -0300

    feat: add get and post route response jsonify handling

--- db.py
+++ db.py
@@ -1,3 +1,5 @@
+from validation import Part
+
 Parts = [
     {
         'id': 1,
--- main.py
+++ main.py
@@ -10,17 +10,24 @@ Step 3: run the app
 from flask import Flask, make_response, jsonify, request
 
 app = Flask(__name__)
+app.config['JSON_SORT_KEYS'] = False 
 
 # Decorator from flask import
 @app.route('/parts', methods=['GET'])
 def get_parts():
     # It's important to use flask make_response function to return a HTTP response. To handle data like raw list its also interesting to use jsonify function function to handle it as a json
-    return make_response(jsonify(Parts))
+    return make_response(jsonify(
+        message='Part list',
+        data=Parts
+    ))
 
 @app.route('/parts/create', methods=['POST'])
 def create_new_part():
     part = request.json
     Parts.append(part)
-    return part
+    return make_response(jsonify(
+        message='Part successfully created!',
+        part=part
+    ))
 
 app.run(debug=True)
